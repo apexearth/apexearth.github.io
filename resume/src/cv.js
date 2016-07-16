b8e08613ec0938a807d927f3a66f9f6866255b88
cjacobs <apexearth@gmail.com>
2016-07-15 18:18:50 -0700

    tweaks after input from John.

--- resume/src/cv.js
+++ resume/src/cv.js
@@ -13,9 +13,21 @@ export default {
         StackOverflow: "https://stackoverflow.com/cv/chris.jacobs",
         "MCP#":        "11821392"
     },
+    highlights:   [
+        "javascript", "JavaScript", "node.js", "Node.js",
+        "c#", "C#", "java", "Java",
+        "python", "Python",
+        "tdd", "unit-testing",
+        "Full Stack Development",
+        "Test/Build/Release Automation",
+        "rest", "RESTful web services",
+        "gulp", "gruntjs",
+        "scalable",
+        "high performance"
+    ],
     statement:    [
                       "Over 10 years of success driving full lifecycle development, enhancement, and implementation of state-of-the-art software and web applications to meet global business demands.",
-                      "- Proven JavaScript, C#, and SQL expert with ‘multi-hat’ qualifications and advanced skills honed in Full Stack Engineering, Relational Database Design, Object Oriented Programming, Testing, and Automation.",
+                      "- Proven JavaScript, C#, and SQL expert with ‘multi-hat’ qualifications and advanced skills honed in Full Stack Development, Relational Database Design, Object Oriented Programming, Testing, and Automation.",
                       "- Successful in partnering with stakeholders, managers, vendors and teams to define and translate business requirements into scalable and reliable enterprise solutions that yield measurable results in efficiency, productivity, and bottom line profits.",
                       "- Excel at driving on time, within budget delivery of software projects from conception to successful conclusion in fast-paced, deadline-driven environments.",
                       "- Respected as an energetic and motivational leader with talent for mentoring and coaching teams to new levels of performance.",
@@ -70,16 +82,19 @@ export default {
             companyTitle: "Toshiba America Electronics Corporation",
             positions:    [
                 {
-                    title:       "Manufacturing Test Manager",
+                    title:       "Software Engineering Manager - Manufacturing Test",
                     location:    "San Jose, CA",
-                    date:        {start: "June 2015", end: "current"},
+                    date:        {start: "June 2015", end: "July 2016"},
                     tags:        [
-                        "javascript", "sql-server", "c#", "python", "node.js", "mocha", "asp.net", "entity-framework", "web-api", "rest", "lua", "linux", "bash", "git"
+                        "javascript", "sql-server", "c#", "python", "node.js", "mocha", "git", "reactjs", "gruntjs", "gulp",
+                        "tdd", "unit-testing", "asp.net", "entity-framework", "web-api", "rest", "lua", "linux", "bash"
                     ],
                     statement:   [
-                                     "Hands-on leadership of the manufacturing test software team, responsible for manufacturing test of SATA & NVMe SSDs.",
-                                     "Work directly with product designers to gather enterprise level testing requirements for cutting-edge technologies.",
-                                     "Architect advanced software, systems, and networking solutions to meet manufacturing test requirements.",
+                                     "Widely ***respected as a technical leader***, who turned a troubled software engineering team into a successful, dynamic unit.",
+                                     "- Architect advanced software, systems, and networking solutions to meet manufacturing test requirements.",
+                                     "  Fusing many different technologies together such as Python, Node.js, C#, C++, RESTful web services, React.js, SQL Server, utilizing each for their inherent advantages.",
+                                     "- Work directly with product designers to gather enterprise level testing requirements for cutting-edge technologies.",
+                                     "- A skilled, hands-on leader, responsible for the manufacturing testing of SATA & NVMe SSDs.",
                                  ].join(" \n"),
                     description: [
                                      "### Selected Achievements:",
@@ -90,10 +105,12 @@ export default {
                                      "        - Forward-looking design through integration, automation, and high maintainability.",
                                      "    - Key contributor to coordinating, data mining, and improving the quality of RDT efforts.",
                                      "    - Worked with manufacturers to refine and provide optimal testing solutions.",
-                                     "- Overhauled the software release management process by adding peer code reviews, documentation, and automating release deployments.",
-                                     "    - Setup a GitLab server to manage builds, releases and issues.",
+                                     "- Overhauled the software development & release management processes by adding peer code reviews, documentation, and automating release deployments.",
+                                     "    - Setup a GitLab server to manage builds, testing, releases and issues.",
                                      "    - Oversaw the completion of dozens of releases and hundreds of issues.",
                                      "    - Drove initiatives to automate builds and releases wherever appropriate, increase the overall efficiency of the team.",
+                                     "    - Added unit-testing requirements for all written software, improving software quality and maintainability.",
+                                     "    - Encouraged and rewarded team-members to research, study, and adopt industry best-practices at all times.",
                                  ].join(" \n")
                 }
             ]
@@ -104,9 +121,9 @@ export default {
                 {
                     title:       "Senior Software Engineer",
                     location:    "San Jose, CA",
-                    date:        {start: "November 2006", end: "June 2015"},
+                    date:        {start: "November 2010", end: "June 2015"},
                     tags:        [
-                        "javascript", "c#", "sql", "sql-server", "node.js", "angularjs", "orm", "entity-framework", "model-view-controller", "data-modeling", "rest", "asp.net", "html", "css"
+                        "javascript", "c#", "sql", "sql-server", "node.js", "git", "gruntjs", "gulp", "tdd", "unit-testing", "java", "angularjs", "orm", "entity-framework", "model-view-controller", "data-modeling", "rest", "web-api", "asp.net", "html", "css"
                     ],
                     statement:   [
                                      "Lead and execute advanced design, development, programming, and implementation of high performance software and web applications to meet evolving business and data management needs.",
@@ -119,27 +136,47 @@ export default {
                                      "- Praised for implementing best practice methodologies to ensure on-time delivery of scalable, value-adding software solutions with a compelling ROI.",
                                      "- Maintained high levels of stability/reliability through skillful management of development lifecycle and definition of repeatable process methodologies.",
                                      "- Entrusted as a high-impact coach and mentor to junior team members, equipping them with the skills, tools, and strategies to meet ambitious service level deliverables.",
-                                     "- Initially hired as Software Developer; assumed additional responsibilities of System Administrator, effectively fulfilling dual roles for nearly 2 years to accommodate challenging business needs during period of rapid corporate growth and expansion.",
                                      "   \n   \n",
                                      "### Notable Projects:",
-                                     "- Designed, developed, and integrated web-based order approval application with Sage ERP system, nearly eliminating use of paper during order approval process. Technologies included SQL Server, Entity Framework, ASP.NET MVC, and Bootstrap.",
+                                     "- Designed, developed, and integrated web-based order approval application with Sage ERP system, nearly eliminating use of paper during order approval process. Technologies included JavaScript, C#, SQL Server, Entity Framework, ASP.NET MVC, and Bootstrap.",
                                      "    - Slashed touch time by 75% while accelerating cycle time by 35%.",
-                                     "- Spearheaded and directed collaborative initiative with Product Engineers to build flexible database design that enabled management of test configurations via web interface and storage of test results for numerous manufacturers and databases. Technologies included SQL Server, C#, Entity Framework, ASP.NET Web API, AngularJS, and Bootstrap.",
+                                     "- Spearheaded and directed collaborative initiative with Product Engineers to build flexible database design that enabled management of test configurations via web application and storage of test results for numerous manufacturers and databases. Technologies included Java, JavaScript, SQL Server, C#, Entity Framework, ASP.NET Web API, AngularJS, and Bootstrap.",
                                      "    - Implemented 4 FlexStar systems in Taiwan facility and trained team leads on system usage.",
                                      "    - Boosted test capacity by more than 30% while significantly expanding analytical capabilities.",
                                      "    - Additionally positioned company for successful entry into the Enterprise SSD market.",
-                                     "- Elevated manufacturing efficiency through the design and implementation of SSD configuration and logging databases with REST APIs and management platform.",
+                                     "- Elevated manufacturing efficiency through the design and implementation of SSD configuration and logging databases with RESTful web services and management platform.",
                                      "    - Increased accuracy and analytical capabilities while expanding factory capacity for processing next-generation SSD products by 20%.",
                                      "- Championed and won approval to develop and implement Master Data System to increase data quality and improve ETL capabilities. Successfully completed project as part of larger, enterprise-wide initiative to increase awareness of the importance of good data practices across all departments and levels.",
                                      "- Key contributor to planning and seamless transition of Taiwan factory operations, systems, and processes to new ownership with zero unplanned down-times.",
                                      "- Hand-picked for high profile, large-scale project to automate key functions on-site at company’s Taiwan manufacturing facility.",
                                      "- Engineered applications to automate product label printing and streamline packaging process, leading to ~95% reduction in print errors and ~15% increase in efficiency.",
+                                 ].join(" \n")
+                }
+            ]
+        },
+        {
+            companyTitle: "OCZ Storage Solutions",
+            positions:    [
+                {
+                    title:       "Software Engineer",
+                    location:    "San Jose, CA",
+                    date:        {start: "November 2006", end: "November 2010"},
+                    tags:        [
+                        "javascript", "c#", "sql", "sql-server", "mercurial", "unit-testing", "orm", "entity-framework", "model-view-controller", "data-modeling", "rest", "web-api", "asp.net", "html", "css"
+                    ],
+                    statement:   [
+                                     "Initially hired as Software Developer; assumed additional responsibilities of System Administrator, effectively fulfilling dual roles for nearly 2 years to accommodate challenging business needs during period of rapid corporate growth and expansion."
+                                 ].join(" \n"),
+                    description: [
+                                     "### Notable Projects:",
                                      "- Led development, launch, and maintenance of website for documentation and tracking of product repair actions and results. Teamed with BI Analysts to create SQL queries for repair data analysis.",
                                      "    - Produced 15% increase in throughput and provided critical data and business intelligence for decision-makers.",
                                      "- Designed, developed, and integrated Materials Planning System with Sage ERP; enabled automated management of testing, manufacturing, and logistics processes for memory products. Fueled impressive 75% increase in processing efficiency.",
-                                     "- Managed full lifecycle development of website for administration of RMA transactions. Led to dramatic increase in transaction accuracy and ~60% decrease in RMA processing time.",
+                                     "- Managed full lifecycle development of website for administration of RMA transactions.",
+                                     "    - Led to dramatic increase in transaction accuracy and ~60% decrease in RMA processing time.",
                                      "- Leveraged deep understanding and earlier hands-on experience in Windows Server maintenance and networking best practices to intuitively tailor software development and deployment plans that consistently produce highest possible levels of performance, compatibility, and functionality.",
                                      "- Designed and developed customized client-server reporting system generating dozens of reports vital to core business functions company-wide.",
+                                     "- Developed and maintained a suite of many ERP-integrated applications supporting global operations, increasing the efficiency of day-to-day actions in all facets of the company."
                                  ].join(" \n")
                 }
             ]
--- resume/src/index.js
+++ resume/src/index.js
@@ -1,4 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './Root.js'
+import cv from './cv'
+
 ReactDOM.render(<Root />, document.getElementById("root"));
+
+if (cv.highlights && cv.highlights.length) {
+    for (var i = 0; i < cv.highlights.length; i++) {
+        document.body.innerHTML =
+            document.body.innerHTML.replace(
+                new RegExp(cv.highlights[i], "g"),
+                "<span style='" +
+                "font-weight: bold; " +
+                "display: inline-block; " +
+                "background-color: rgb(245,255,235); " +
+                "padding: 2px; " +
+                "margin: -2px; " +
+                "-webkit-print-color-adjust: exact;" +
+                "'>" + cv.highlights[i] + "</span>")
+    }
+}
